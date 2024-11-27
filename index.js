d48c694c8b8aa7db32514b6d4bb0017e756e5f77
Ezra <ezraraddai166@gmail.com>
2024-11-27 10:40:03 +0300

    firstcommit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "currentUser"
+let welcomeMessage =`Welcome to Flatbook, ${currentUser}!`
+const CURRENTUSER = "CURRENTUSER"
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${CURRENTUSER}!`
+let shortGreeting = "Welcome, !"
